f880d0b1b782bd16aee30856df56d3b20087b3c0
Imran Ansari <imran.ansari2@globallogic.com>
2025-10-05 18:42:20 +0530

    rest api completed

--- productlist/src/main/java/com/bitsandbytes/product/controller/ProductController.java
+++ productlist/src/main/java/com/bitsandbytes/product/controller/ProductController.java
@@ -1,16 +1,49 @@
 package com.bitsandbytes.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bitsandbytes.product.dto.ProductDTO;
+import com.bitsandbytes.product.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
 
+    private ProductService productService;
+
     // get products
+    @GetMapping
+    public List<ProductDTO> getAllProduct(){
+        return productService.getAllProducts();
+    }
     // create products
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        ProductDTO createProduct = productService.createProduct(productDTO);
+        return new ResponseEntity<>(createProduct, HttpStatus.CREATED);
+    }
     // update product
+
+    @PutMapping("/{id}")
+    public ProductDTO updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO){
+        return productService.updateProduct(id,productDTO);
+    }
     // get product by id
+    @GetMapping("/{id}")
+    public ProductDTO getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
     // delete product
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id){
+
+        return productService.deleteProductById(id);
+    }
 
 }
--- productlist/src/main/java/com/bitsandbytes/product/service/CategoryService.java
+++ productlist/src/main/java/com/bitsandbytes/product/service/CategoryService.java
@@ -8,6 +8,8 @@ import com.bitsandbytes.product.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CategoryService {
@@ -22,7 +24,19 @@ public class CategoryService {
         category = categoryRepository.save(category);
         return CategoryMapper.toCategoryDTO(category);
     }
+
     // get all categories
+    public List<CategoryDTO> getAllCategories(){
+        return categoryRepository.findAll().stream().map(CategoryMapper::toCategoryDTO).toList();
+    }
     // get category by id;
+    public CategoryDTO getCategoryById(Long id){
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category not found"));
+        return CategoryMapper.toCategoryDTO(category);
+    }
     // delete category
+    public String deleteCategory(Long id){
+        categoryRepository.deleteById(id);
+        return "Category"+id+" has been deleted!";
+    }
 }
